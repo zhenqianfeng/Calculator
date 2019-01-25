60ab3019883ab1e429996be9cfacf37cc8254610
zhenqianfeng <ferretfeng@yahoo.ca>
2019-01-24 21:35:53 -0500

    Initial commit

---
+++ app/src/main/java/com/concordia/cejv669/calculator/MainActivity.java
@@ -0,0 +1,37 @@
+package com.concordia.cejv669.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1=findViewById(R.id.btn_add);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //               int num1 = Integer.parseInt(editNum1.getText().toString());
+
+                EditText n1=findViewById(R.id.number1);
+                int num1 = Integer.parseInt(n1.getText().toString());
+
+                EditText n2=findViewById(R.id.number2);
+                int num2 = Integer.parseInt(n2.getText().toString());
+
+                int r = num1 + num2;
+                TextView tv1 = findViewById(R.id.result);
+
+                tv1.setText(""+r);
+            }
+        });
+    }
+}
